ceb85a5ffc8c2e8da261b628cdebeab6323c0366
Dogo <47029530+dogoterra@users.noreply.github.com>
2021-07-02 03:30:03 +0200

    Update

---
+++ mail.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const infoBox = document.getElementById("mail");
+    const mail = atob("ZG9nb0Bkb3NvLmVz");
+    
+    infoBox.innerText = `More Info: ${mail}`;
+})
